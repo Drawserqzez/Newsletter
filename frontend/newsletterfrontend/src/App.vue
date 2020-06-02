2a092b25d3a2aa46d85069acecf4a1b780ee0218
unknown <drawserqzez@gmail.com>
2020-06-02 17:08:23 +0200

    First work with vue.js

---
+++ frontend/newsletterfrontend/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Navbar home="home" login="login"/>
+    <MainContent/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <HelloWorld msg="Din mammaaaaaaa"/> -->
+  </div>
+</template>
+
+<script>
+import Navbar from './components/NavBar.vue'
+import MainContent from './components/MainContent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    MainContent
+  }
+}
+
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%; 
+  height: 100%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  width: 100%;
+  height:100%;
+}
+</style>
---
+++ frontend/newsletterfrontend/src/components/NavBar.vue
@@ -0,0 +1,59 @@
+<template>
+    <nav>
+        <div class="middle">
+            <ul>
+                <Navitem header="Newsletter" onClick="home"/>
+                <Navitem header="Logga in" onClick="login"/>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    import Navitem from './Navitem';
+
+    export default {
+        name: 'Navbar',
+        components: {
+            Navitem
+        },
+        props: {
+            home: String,
+            login: String
+        }
+    }
+</script>
+
+<style scoped>
+
+nav {
+    background-color: lightgreen;
+    color:black;
+
+    padding: 40px;
+}
+
+ul {
+    list-style: none;
+    margin: 0;
+    /* display: flexbox;
+    flex-direction: row;
+    justify-content: center; */
+}
+
+li {
+    display: flex;
+    justify-self: center;
+    float: left;
+    margin-top: -20px;
+    margin-left: -50px;
+    margin-right: 45px;
+}
+
+/* .middle {
+    width: 10%;
+    margin-left: auto;
+    margin-right: auto;
+} */
+
+</style>
